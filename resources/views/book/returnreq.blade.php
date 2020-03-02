3760b0aca9b3410159741410fc03c85ba0f95502
kaz-sima <simanaka@sage.ocn.ne.jp>
2020-03-02 20:19:28 +0630

    BorrowBooks

---
+++ resources/views/book/returnreq.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.userlayout')
+@section('content')
+@include('common.errors')
+<div class="col-md-12">
+ <p>You have already made a reservation.</p>
+ <p>Please release the reservation first.</p>
+</div>
+@endsection
