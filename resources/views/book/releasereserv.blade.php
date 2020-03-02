e48fb7ff3f9860050c66d598e4f4e6e52caf7a72
kaz-sima <simanaka@sage.ocn.ne.jp>
2020-03-02 20:19:28 +0630

    BorrowBooks

---
+++ resources/views/book/releasereserv.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.userlayout')
+@section('content')
+@include('common.errors')
+<div class="col-md-12">
+ <p>You have reached the limit.</p>
+ <p>Please return the book first.</p>
+</div>
+@endsection
+
