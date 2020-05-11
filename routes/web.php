0ec7d66202a908be70b19f69d8798baea18c0d50
kaz-sima <simanaka@sage.ocn.ne.jp>
2020-05-12 00:10:02 +0900

    Applying Google Map

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('/'); // name the route for multi logins in advance
-
+Route::get('about', function () {
+    return view('about');
+});
 Route::prefix('admin')->namespace('Admin')->as('admin.')->group(function(){
     Route::middleware('guest:admin')->group(function(){
             
