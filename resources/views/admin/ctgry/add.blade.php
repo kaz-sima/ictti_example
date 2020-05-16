kaz-sima/test

CategoryManagement

resources/views/admin/ctgry/add.blade.php
@@ -1,0 +1,47 @@
+@extends('layouts.layout')
+@section('content')
+	<div id="form_page">
+		<div class="container">
+			<div class="home-list-header">
+				<div class="row">
+					<div class="col-4 offset-4">
+						<h2>Add Ctgry & Sub Ctgry</h2>
+					</div>
+				</div>
+			</div>
+            <div class="panel panel-default">
+            <div class="panel-heading"><strong>Add Ctgry</strong></div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                <form action="{{url('admin/ctgry/add')}}" method="POST">
+                {{csrf_field()}}
+               	<tr><th>ctgry code</th><td><input type="number" name="code"></td></tr>
+					<tr><th>ctgry name</th><td><input type='text' name="name"></td></tr>
+					<tr><th></th><td><input type="submit" value="send"></td></tr></tr>
+				</form>
+				</table>
+			</div>
+			</div>
+			<div class="panel panel-default">
+            <div class="panel-heading"><strong>Add Sub Ctgry</strong></div>
+            <div class="panel-body">
+				<table class="table table-striped task-table">
+                <form action="{{url('admin/subctgry/add')}}" method="POST">
+                {{csrf_field()}}
+                	<tr><th>ctgry code</th><td>
+                	<select name="ctgry_code">
+  					@foreach($ctgries as $ctgry => $name)
+    					<option value="{{ $ctgry }}" {{old('ctgry') == $ctgry ? 'selected': ''}}>{{$name}}</option>
+  					@endforeach
+					</select>
+                	</td></tr>
+                	<tr><th>sub ctgry code</th><td><input type="number" name="code"></td></tr>
+					<tr><th>sub ctgry name</th><td><input type='text' name="name"></td></tr>
+					<tr><th></th><td><input type="submit" value="send"></td></tr></tr>
+				</form>
+                </table>
+			</div>
+			</div>
+		</div>
+	</div>
+@endsection

resources/views/admin/ctgry/index.blade.php
@@ -1,0 +1,37 @@
+@extends('layouts.layout')
+@section('content')
+	<div id="form_page">
+			<div class="container">
+				<div class="home-list-header">
+					<div class="row">
+						<div class="col-4 offset-4">
+							<h2>Current Category List</h2>
+						</div>
+					</div>
+				</div>
+<a href="{{ url('admin/ctgry/add')}}"><strong>To Add New ctgry</strong></a>&nbsp;
+           <div class="panel panel-default">
+           <div class="panel-body">
+                <table class="table table-striped task-table">
+                <tr><th>Ctgry(code)</th><th>Subctgry(code)</th></tr>
+                @foreach ($items as $item)
+                    <tr>
+						<td>{{ $item->getData() }}</td>
+						<td>
+							@if ($item->subctgries != null)
+								<table width="100%">
+								@foreach ($item->subctgries as $obj)
+									<tr><td>{{$obj->getData()}}</td></tr>
+								@endforeach
+								</table>
+							@endif
+						</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
+	</div>
+</div>
+</div>
+@endsection
+

kaz-sima/ictti_example

resources/views/admin/ctgry/add.blade.php
@@ -1,0 +1,47 @@
+@extends('layouts.adminlayout')
+@section('content')
+	<div id="form_page">
+		<div class="container">
+			<div class="home-list-header">
+				<div class="row">
+					<div class="col-4 offset-4">
+						<h2>Add Ctgry & Sub Ctgry</h2>
+					</div>
+				</div>
+			</div>
+            <div class="panel panel-default">
+            <div class="panel-heading"><strong>Add Ctgry</strong></div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                <form action="{{url('admin/ctgry/add')}}" method="POST">
+                {{csrf_field()}}
+               	<tr><th>ctgry code</th><td><input type="number" name="code"></td></tr>
+					<tr><th>ctgry name</th><td><input type='text' name="name"></td></tr>
+					<tr><th></th><td><input type="submit" value="send"></td></tr></tr>
+				</form>
+				</table>
+			</div>
+			</div>
+			<div class="panel panel-default">
+            <div class="panel-heading"><strong>Add Sub Ctgry</strong></div>
+            <div class="panel-body">
+				<table class="table table-striped task-table">
+                <form action="{{url('admin/subctgry/add')}}" method="POST">
+                {{csrf_field()}}
+                	<tr><th>ctgry code</th><td>
+                	<select name="ctgry_code">
+  					@foreach($ctgries as $ctgry => $name)
+    					<option value="{{ $ctgry }}" {{old('ctgry') == $ctgry ? 'selected': ''}}>{{$name}}</option>
+  					@endforeach
+					</select>
+                	</td></tr>
+                	<tr><th>sub ctgry code</th><td><input type="number" name="code"></td></tr>
+					<tr><th>sub ctgry name</th><td><input type='text' name="name"></td></tr>
+					<tr><th></th><td><input type="submit" value="send"></td></tr></tr>
+				</form>
+                </table>
+			</div>
+			</div>
+		</div>
+	</div>
+@endsection

resources/views/admin/ctgry/index.blade.php
@@ -1,0 +1,37 @@
+@extends('layouts.adminlayout')
+@section('content')
+	<div id="form_page">
+			<div class="container">
+				<div class="home-list-header">
+					<div class="row">
+						<div class="col-4 offset-4">
+							<h2>Current Category List</h2>
+						</div>
+					</div>
+				</div>
+<a href="{{ url('admin/ctgry/add')}}"><strong>To Add New ctgry</strong></a>&nbsp;
+           <div class="panel panel-default">
+           <div class="panel-body">
+                <table class="table table-striped task-table">
+                <tr><th>Ctgry(code)</th><th>Subctgry(code)</th></tr>
+                @foreach ($items as $item)
+                    <tr>
+						<td>{{ $item->getData() }}</td>
+						<td>
+							@if ($item->subctgries != null)
+								<table width="100%">
+								@foreach ($item->subctgries as $obj)
+									<tr><td>{{$obj->getData()}}</td></tr>
+								@endforeach
+								</table>
+							@endif
+						</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
+	</div>
+</div>
+</div>
+@endsection
+